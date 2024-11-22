c74821f7acfab5b9a1a392411c4201f41bffb613
Bruno M. Cerqueira <bruno.cerqueira_ext@sonepar.com.br>
2024-11-22 14:59:12 -0300

    Aula 2.4 react-hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 export default function App() {
   console.log('Teste no console do navegador');
+
   const name = 'Bruno Romeo'
   const age = '38'
 
   return (
     <>
-      <header>
-        <div className="bg-green-300 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            React Hello
-          </h1>
-        </div>
-      </header>
+      <Header size='large'>
+        Componente Header - React Hello
+      </Header>
 
-      <main>
+      <Main>
         <div className="container mx-auto p-4">
           <p>
-            O seu nome é {name}, com {name.length} caracteres, e você possui {age} anos
+            O seu nome é Bruno, com 11 caracteres, e você possui 38 anos
           </p>
         </div>
-      </main>
+      </Main>
     </>
   );
 }
