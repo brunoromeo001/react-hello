a3355ba9ad5cbfdb91ee7f3ed9d81df615100778
Bruno M. Cerqueira <bruno.cerqueira_ext@sonepar.com.br>
2024-11-25 08:54:48 -0300

    Aula 2.6 react-hello

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
   console.log('Teste no console do navegador');
+  const state = useState('Bruno');
+
+  const [name, setName] = useState('Bruno');
+
+  function handleNameChange(event) {
+    const newName = event.currentTarget.value
+
+    setName(newName)
+  }
 
   return (
     <>
@@ -11,12 +21,24 @@ export default function App() {
       </Header>
 
       <Main>
-        <input
-          type='text'
-          className="border"
-        />
+        <div className="flex flex-col my-4">
+          <label 
+            className="text-sm mb-1" 
+            htmlFor="inputName"
+          >
+            Digite o seu nome: 
+
+          </label>
+          <input
+            id="inputName"
+            type='text'
+            className="border"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
         <p>
-          O seu nome é Bruno, com 11 caracteres, e você possui 38 anos
+          O seu nome é {name}, com {name.length} caracteres, e você possui 38 anos
         </p>
       </Main>
 
